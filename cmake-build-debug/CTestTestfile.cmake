1651f3925444ae112650018cf1de49c53c8d3173
BoboBaggins <jaakkokuusisto@hotmail.com>
2025-02-09 19:47:59 +0200

    Removed nested repository

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/BoboBaggins/CLionProjects/myLogger
+# Build directory: /home/BoboBaggins/CLionProjects/myLogger/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[LoggerTests]=] "/home/BoboBaggins/CLionProjects/myLogger/cmake-build-debug/myLoggerTests")
+set_tests_properties([=[LoggerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/BoboBaggins/CLionProjects/myLogger/CMakeLists.txt;26;add_test;/home/BoboBaggins/CLionProjects/myLogger/CMakeLists.txt;0;")
